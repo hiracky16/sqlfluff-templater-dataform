7cba156177872e19c35700c1637f1ff34e8437c6
moreaupascal56 <moreaupascal56@gmail.com>
2025-04-21 16:21:24 +0100

    Adds support for pre and post operations

--- test/templater_test.py
+++ test/templater_test.py
@@ -1,4 +1,5 @@
 """Tests for the dataform templater."""
+from pytest import mark
 
 def test_has_js_block(templater):
     has_js_sql = """config {
@@ -53,25 +54,69 @@ def test_replace_ref_with_bq_table_multiple_refs(templater):
     result = templater.replace_ref_with_bq_table(input_sql)
     assert result == expected_sql
 
-def test_replace_blocks_single_block(templater):
-    input_sql = """config {
-    type: "table",
-    columns: {
-        "test" : "test",
-        "value:: "value"
-    }
-}
-SELECT * FROM my_table"""
 
-    expected_sql = "\nSELECT * FROM my_table"
+@mark.parametrize(
+    "test_block, replaced_sql",
+    [
+        (
+                "",
+                ""
+        ),
+        (
+                """config {
+                type: "table",
+                columns: {
+                    "test" : "test",
+                    "value:: "value"
+                }
+                }""",
+                ""
+        ),
+        (
+                """pre_operations {
+                CREATE TEMP FUNCTION AddFourAndDivide(x INT64, y INT64)
+                    RETURNS FLOAT64
+                    AS ((x + 4) / y);
+                }""",
+                ""
+        ),
+        (
+                """post_operations {
+                GRANT `roles/bigquery.dataViewer`
+                ON
+                TABLE ${self()}
+                TO "group:allusers@example.com", "user:otheruser@example.com"
+                }""",
+                ""
+        ),
+        (
+                """config {
+                type: "table",
+                columns: {
+                    "test" : "test",
+                    "value:: "value"
+                }
+                } pre_operations {
+                CREATE TEMP FUNCTION AddFourAndDivide(x INT64, y INT64)
+                    RETURNS FLOAT64
+                    AS ((x + 4) / y);
+                } post_operations {
+                GRANT `roles/bigquery.dataViewer`
+                ON
+                TABLE ${self()}
+                TO "group:allusers@example.com", "user:otheruser@example.com"
+                }""",
+                "  "
+        ),
+    ]
+)
+def test_replace_blocks(templater, test_block, replaced_sql):
+    sql_body = "SELECT * FROM my_table"
+    input_sql = f"{test_block} {sql_body}"
+    expected_sql = f"{replaced_sql} {sql_body}"
     result = templater.replace_blocks(input_sql)
     assert result == expected_sql
 
-def test_replace_blocks_no_block(templater):
-    input_sql = "SELECT * FROM my_table"
-    expected_sql = "SELECT * FROM my_table"
-    result = templater.replace_blocks(input_sql)
-    assert result == expected_sql
 
 # slice_sqlx_template のテスト
 def test_slice_sqlx_template_with_config_and_ref(templater):
